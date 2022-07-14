f021305f7589b35d2e74a4f55895d899e36ca5fb
guilherme <jguicf@gmail.com>
2022-07-14 01:07:53 -0400

    Implementando SwiftLint

---
+++ TesteDigio/TesteDigio/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  TesteDigio
+//
+//  Created by João Guilherme on 14/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
